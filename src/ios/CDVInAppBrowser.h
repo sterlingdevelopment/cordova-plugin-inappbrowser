10160a8f9d1437c80066186d187641f763fbd283
Josh Raymond <josh.raymond@dishd2d.com>
2019-02-13 17:14:56 -0700

    a

--- src/ios/CDVInAppBrowser.h
+++ src/ios/CDVInAppBrowser.h
@@ -55,8 +55,8 @@
 
 @property (nonatomic, copy) NSString* presentationstyle;
 @property (nonatomic, copy) NSString* transitionstyle;
-@property (nonatomic, assign) NSNumber height;
-@property (nonatomic, assign) NSNumber width;
+@property (nonatomic, assign) NSInteger* height;
+@property (nonatomic, assign) NSInteger* width;
 
 @property (nonatomic, assign) BOOL enableviewportscale;
 @property (nonatomic, assign) BOOL mediaplaybackrequiresuseraction;
